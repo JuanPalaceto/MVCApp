7b8ff3d31e1db1722d313e1d1b6b85812f202980
Juan Palaceto <jpalaceto@hotmail.com>
2023-10-15 16:06:29 -0600

    primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MiSitioWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiSitioWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Si requieres servicios de desarollo de software o tienes preguntas no dudes en contactarme.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(Contacto contacto)
+        {
+            ContactoContext db = new ContactoContext();
+            db.Contactos.Add(contacto);
+
+            try
+            {
+                db.SaveChanges();
+                ViewBag.Message = "Tu mensaje se ha guardado..";
+            }
+            catch (Exception e)
+            {
+                ViewBag.Message = "Error al guardar.";
+            }
+
+           return View();
+        }
+
+        [Authorize]
+        public ActionResult Projects() {
+            return View();
+        }
+    }
+}
